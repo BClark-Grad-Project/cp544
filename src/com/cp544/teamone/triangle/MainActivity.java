b2ff7764b510bbb2410cd5d98289122f1bab6792
Brandon L. Clark <bclark10@csu.fullerton.edu>
2014-09-27 09:46:05 -0700

    Broke down game for better modularity for unit test on the gameTriangleGenerate method.

--- src/com/cp544/teamone/triangle/MainActivity.java
+++ src/com/cp544/teamone/triangle/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cp544.teamone.triangle;
 
 import java.util.Locale;
+
+import android.R.bool;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.Point;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +25,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
+	public UnitTestGameGenerateTriangle unitTest;
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
 	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
@@ -33,14 +38,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
-
-	// set up the variables here
-	int a, b, c;
-	Button gen, clear;
-	EditText input1, input2, input3;
-	String input1a, input2b, input3c;
-	TextView display, display2;
-	ImageView idisplay;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +77,16 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(mSectionsPagerAdapter.getListener(i)));
 		}
+
+		TextView displayName;
+		ImageView displayImage;
+		displayName = (TextView) findViewById(R.id.textView6);
+		displayImage = (ImageView) findViewById(R.id.display);
+		
+		unitTest = new UnitTestGameGenerateTriangle(displayName, displayImage);
+		unitTest.testIsoselese();
+		unitTest.testEquilateral();
+		unitTest.testScalene();
 	}
 
 	@Override
@@ -119,6 +126,107 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 			FragmentTransaction fragmentTransaction) {
 	}
 
+	public class Game {
+		int bottomLength, leftLength, rightLength;
+		Button btnGenerate, btnClear;
+		EditText inputBottom, inputLeft, inputRight;
+		TextView disaplyTriangleName, labelResults;
+		ImageView displayTriangleImage;
+		
+		public void invalidInput(TextView tView, ImageView iView) {
+			tView.setText("Error:Please try again, remember your number must be less than 100");
+			iView.setImageResource(android.R.color.transparent);
+			//Log.d("Triangle Generate", "Clear fields. Sucess!");
+		}
+		
+		public void viewIsosceles(TextView tView, ImageView iView){
+			tView.setText("Isosceles Triangle: Two Congruent Sides");
+			iView.setImageResource(R.drawable.i_triangle);	
+			//Log.d("Triangle Generate", "View Isoseles. Sucess!");
+		}		
+		
+		public void viewEquilateral(TextView tView, ImageView iView){	
+			tView.setText("Equilateral Triangle: All sides are equal");
+			iView.setImageResource(R.drawable.e_triangle);
+			//Log.d("Triangle Generate", "View Equilateral. Sucess!");
+		}	
+		
+		public void viewScalene(TextView tView, ImageView iView){
+			tView.setText("Scalene Triangle: No Congruent Sides");
+			iView.setImageResource(R.drawable.s_triangle);
+			//Log.d("Triangle Generate", "View Scalene. Sucess!");
+		}
+		
+		public void clearFields(View v){
+			if (v.getId() == R.id.button2) {
+				inputBottom.setText("");
+				inputLeft.setText("");
+				inputRight.setText("");
+				disaplyTriangleName.setText("");
+				displayTriangleImage.setImageResource(android.R.color.transparent);
+			}
+		}
+		
+		public int gameGenerateTriangle(TextView tView, ImageView iView, int a, int b, int c) {				
+			if (((a == b && b != c) || (a == c && b != c) || (b == c && a != c))
+					&& (a <= 100 && b <= 100 && c <= 100)) {
+				viewIsosceles(tView, iView);
+				return 2;
+			} else if ((a == b && a == c)
+					&& (a <= 100 && b <= 100 && c <= 100)) {
+				viewEquilateral(tView, iView);
+				return 1;
+			} else if ((a != b && a != c && b != c)
+					&& (a <= 100 && b <= 100 && c <= 100)) {
+				viewScalene(tView, iView);
+				return 3;
+			} else {
+				invalidInput(tView, iView);
+				return 0;
+			}
+		}
+		
+		public void gameLogic(TextView tView, ImageView iView, EditText eText1, EditText eText2, EditText eText3){				
+			// convert text to int
+			bottomLength = Integer.parseInt(eText1.getText().toString());
+			leftLength = Integer.parseInt(eText2.getText().toString());
+			rightLength = Integer.parseInt(eText3.getText().toString());
+
+			// Choose triangle. 
+			gameGenerateTriangle(tView, iView, bottomLength, leftLength, rightLength);			
+		}
+		
+		public void gameLayout(){
+			setContentView(R.layout.game);
+			// assigning the values
+			inputBottom = (EditText) findViewById(R.id.editText1);
+			inputLeft = (EditText) findViewById(R.id.editText2);
+			inputRight = (EditText) findViewById(R.id.editText3);
+			disaplyTriangleName = (TextView) findViewById(R.id.textView6);
+			labelResults = (TextView) findViewById(R.id.textView5);
+			btnGenerate = (Button) findViewById(R.id.button1);
+			btnClear = (Button) findViewById(R.id.button2);
+			displayTriangleImage = (ImageView) findViewById(R.id.display);
+
+			// setting up Gen button
+			btnGenerate.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {								 
+					gameLogic(disaplyTriangleName, displayTriangleImage, 
+							inputBottom, inputLeft, inputRight);
+				}
+			});
+
+			// setting up the clear button
+			btnClear.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					clearFields(v);
+				}
+			});
+		}
+	}
+	
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
@@ -143,87 +251,26 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 			return 3;
 		}
 		
-		public void invalidInput() {
-			display = (TextView) findViewById(R.id.textView5);
-			idisplay = (ImageView) findViewById(R.id.display);
-			display.setText("Error:Please try again, remember your number must be less than 100");
-			idisplay.setImageResource(android.R.color.transparent);
-		}	
+		
+		public void selectTabLogic(ActionBar.Tab tab, FragmentTransaction ft){			
+			// show the given tab
+			if (tab.getPosition() == 0) {
+				Game game = new Game(); 
+				game.gameLayout();
+			}
+			if (tab.getPosition() == 1) {
+				setContentView(R.layout.guide);
+			}
+			if (tab.getPosition() == 2) {
+				setContentView(R.layout.about);
+			}
+		}
 		
 		public ActionBar.TabListener getListener(int position) {
 			return new ActionBar.TabListener() {
 				public void onTabSelected(ActionBar.Tab tab,
-						FragmentTransaction ft) {					
-					// show the given tab
-					if (tab.getPosition() == 0) {
-						setContentView(R.layout.game);
-						// assigning the values
-						input1 = (EditText) findViewById(R.id.editText1);
-						input2 = (EditText) findViewById(R.id.editText2);
-						input3 = (EditText) findViewById(R.id.editText3);
-						display = (TextView) findViewById(R.id.textView5);
-						display2 = (TextView) findViewById(R.id.textView6);
-						gen = (Button) findViewById(R.id.button1);
-						clear = (Button) findViewById(R.id.button2);
-						idisplay = (ImageView) findViewById(R.id.display);
-
-						// setting up Gen button
-						gen.setOnClickListener(new View.OnClickListener() {
-
-							@Override
-							public void onClick(View v) {								 
-								// getting the text and converting it to string value
-								input1a = input1.getText().toString();
-								input2b = input2.getText().toString();
-								input3c = input3.getText().toString();
-								
-								// converting those text values back into int
-								a = Integer.parseInt(input1a);
-								b = Integer.parseInt(input2b);
-								c = Integer.parseInt(input3c);
-				
-								// setting images
-								if (((a == b && b != c) || (a == c && b != c) || (b == c && a != c))
-										&& (a <= 100 && b <= 100 && c <= 100)) {
-									display.setText("Isosceles Triangle: Two Congruent Sides");
-									idisplay.setImageResource(R.drawable.i_triangle);
-								} else if ((a == b && a == c)
-										&& (a <= 100 && b <= 100 && c <= 100)) {
-									display.setText("Equilateral Triangle: All sides are equal");
-									idisplay.setImageResource(R.drawable.e_triangle);
-								} else if ((a != b && a != c && b != c)
-										&& (a <= 100 && b <= 100 && c <= 100)) {
-									display.setText("Scalene Triangle: No Congruent Sides");
-									idisplay.setImageResource(R.drawable.s_triangle);
-								} else {
-									invalidInput();
-								}
-							}
-						});
-
-						// setting up the clear button
-
-						clear.setOnClickListener(new View.OnClickListener() {
-
-							@Override
-							public void onClick(View v) {
-								// TODO Auto-generated method stub
-								if (v.getId() == R.id.button2) {
-									input1.setText("");
-									input2.setText("");
-									input3.setText("");
-									display.setText("");
-									idisplay.setImageResource(android.R.color.transparent);
-								}
-							}
-						});
-					}
-					if (tab.getPosition() == 1) {
-						setContentView(R.layout.guide);
-					}
-					if (tab.getPosition() == 2) {
-						setContentView(R.layout.about);
-					}
+						FragmentTransaction ft) {	
+					selectTabLogic(tab, ft);
 				}
 
 				public void onTabUnselected(ActionBar.Tab tab,
@@ -285,4 +332,168 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 			return rootView;
 		}
 	}
+	
+	public class UnitTestGameGenerateTriangle {
+		// Outside Bounds returns 0
+		// Isoselese returns 1
+		// Equilateral returns 2
+		// Scalene returns 3
+		Game game = new Game();
+		TextView tView;
+		ImageView iView;
+		// Bounds
+		Point upperLower = new Point(0, 100);
+		Point belowAbove = new Point(-1, 101);
+		int center = 50;
+		
+		public UnitTestGameGenerateTriangle(TextView tv, ImageView iv){
+			this.tView = tv;
+			this.iView = iv;
+		}
+		
+		public void testIsoselese(){
+			Log.d("(TI) Isoseles Boundry Test", "Begin");			
+			TIupper();
+			TIlower();
+			TIbelow();
+			TIabove();
+			TImiddle();
+			Log.d("(TI) Isoseles Boundry Test", "End");
+		}
+		public void testEquilateral(){
+			Log.d("(TE) Equilateral Boundry Test", "Begin");
+			TEupper();
+			TElower();
+			TEbelow();
+			TEabove();
+			TEmiddle();
+			Log.d("(TE) Equilateral Boundry Test", "End");
+		}
+		public void testScalene(){
+			Log.d("(TS) Scalene Boundry Test", "Begin");
+			TSupper();
+			TSlower();
+			TSbelow();
+			TSabove();
+			TSmiddle();
+			Log.d("(TS) Scalene Boundry Test", "End");
+		}
+		int value;
+		// (TI) Test Isoselese
+		public void TIupper(){
+			value = game.gameGenerateTriangle(tView, iView, upperLower.y, upperLower.y, upperLower.y);
+			if(value == 1){
+				Log.d("TI Upper Bounds", "Passed!");
+			} else {				
+				Log.e("TI Upper Bounds", "Did not pass.");
+			}
+		}
+		public void TIlower(){
+			value = game.gameGenerateTriangle(tView, iView, upperLower.x, upperLower.x, upperLower.x);
+			if(value == 1){
+				Log.d("TI Lower Bounds", "Passed!");
+			} else {				
+				Log.e("TI Lower Bounds", "Did not pass.");
+			}			
+		}
+		public void TIbelow(){
+			value = game.gameGenerateTriangle(tView, iView, belowAbove.x, belowAbove.x, belowAbove.x);
+			if(value == 0){
+				Log.d("TI Below Bounds", "Passed!");
+			} else {				
+				Log.e("TI Below Bounds", "Did not pass.");
+			}			
+		}
+		public void TIabove(){
+			value = game.gameGenerateTriangle(tView, iView, belowAbove.y, belowAbove.y, belowAbove.y);
+			if(value == 0){
+				Log.d("TI Above Bounds", "Passed!");
+			} else {				
+				Log.e("TI Above Bounds", "Did not pass.");
+			}			
+		}
+		public void TImiddle(){
+			value = game.gameGenerateTriangle(tView, iView, center, center, center);
+			if(value == 1){
+				Log.d("TI In Bounds", "Passed!");
+			} else {				
+				Log.e("TI In Bounds", "Did not pass.");
+			}			
+		}
+		
+		// (TE) Test Equilateral
+		public void TEupper(){
+			if(game.gameGenerateTriangle(tView, iView, upperLower.y, upperLower.y, center) == 2){
+				Log.d("TE Upper Bounds", "Passed!");
+			} else {				
+				Log.e("TE Upper Bounds", "Did not pass.");
+			}
+		}
+		public void TElower(){
+			if(game.gameGenerateTriangle(tView, iView, upperLower.x, upperLower.x, center) == 2){
+				Log.d("TE Lower Bounds", "Passed!");
+			} else {				
+				Log.e("TE Lower Bounds", "Did not pass.");
+			}			
+		}
+		public void TEbelow(){
+			if(game.gameGenerateTriangle(tView, iView, belowAbove.x, belowAbove.x, center) == 0){
+				Log.d("TE Below Bounds", "Passed!");
+			} else {				
+				Log.e("TE Below Bounds", "Did not pass.");
+			}			
+		}
+		public void TEabove(){
+			if(game.gameGenerateTriangle(tView, iView, belowAbove.y, belowAbove.y, center) == 0){
+				Log.d("TE Above Bounds", "Passed!");
+			} else {				
+				Log.e("TE Above Bounds", "Did not pass.");
+			}			
+		}
+		public void TEmiddle(){
+			if(game.gameGenerateTriangle(tView, iView, center + 25, center, center) == 2){
+				Log.d("TE In Bounds", "Passed!");
+			} else {				
+				Log.e("TE In Bounds", "Did not pass.");
+			}			
+		}
+
+		// (TS) Test Scalene
+		public void TSupper(){
+			if(game.gameGenerateTriangle(tView, iView, upperLower.y, center + 25, center) == 3){
+				Log.d("TS Upper Bounds", "Passed!");
+			} else {				
+				Log.e("TS Upper Bounds", "Did not pass.");
+			}
+		}
+		public void TSlower(){
+			if(game.gameGenerateTriangle(tView, iView, upperLower.x, center + 25, center) == 3){
+				Log.d("TS Lower Bounds", "Passed!");
+			} else {				
+				Log.e("TS Lower Bounds", "Did not pass.");
+			}			
+		}
+		public void TSbelow(){
+			if(game.gameGenerateTriangle(tView, iView, belowAbove.x, center + 25, center) == 0){
+				Log.d("TS Below Bounds", "Passed!");
+			} else {				
+				Log.e("TS Below Bounds", "Did not pass.");
+			}			
+		}
+		public void TSabove(){
+			if(game.gameGenerateTriangle(tView, iView, belowAbove.y, center + 25, center) == 0){
+				Log.d("TS Above Bounds", "Passed!");
+			} else {				
+				Log.e("TS Above Bounds", "Did not pass.");
+			}			
+		}
+		public void TSmiddle(){
+			if(game.gameGenerateTriangle(tView, iView, center + 25, center - 25, center) == 3){
+				Log.d("TS In Bounds", "Passed!");
+			} else {				
+				Log.e("TS In Bounds", "Did not pass.");
+			}			
+		}
+	}
+
 }
